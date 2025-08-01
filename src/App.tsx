346921f740e8848fb8b5cb1f052f3ac13f270d32
Franco Gatica <fg250107@ncr.com>
2025-07-31 20:30:10 -0400

    Inicialización de proyecto F-Report Frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-blue-500 text-white p-6 rounded-xl shadow-lg">
+        🚀 Tailwind v3 funcionando correctamente
+      </div>
+    </div>
+  );
+}
